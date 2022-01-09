296e58200d5068f262a2725281a0fe68a77fbc59
Jon Parise <jon@indelible.org>
2022-01-08 18:10:57 -0800

    Convert test_chars.py to use pytest (#28)

--- tests/test_chars.py
+++ tests/test_chars.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 
 from vesta.chars import CHARCODES
 from vesta.chars import CHARMAP
@@ -10,219 +10,185 @@ from vesta.chars import encode_row
 from vesta.chars import encode_text
 
 
-class EncodeTests(unittest.TestCase):
-    def test_printable_characters(self):
-        for c in CHARMAP:
-            encode(c)
-
-    def test_character_codes(self):
-        for i in CHARCODES:
-            encode(f"{{{i}}}")
-
-    def test_bad_characters(self):
-        self.assertRaisesRegex(ValueError, "unsupported character: <", encode, "<>")
-        self.assertRaisesRegex(
-            ValueError, "unsupported character code: 99", encode, "{99}"
-        )
-        self.assertRaisesRegex(ValueError, "missing }", encode, "{20")
-        self.assertRaisesRegex(ValueError, "missing }", encode, "{999}")
+def test_encode_printable_characters():
+    for c in CHARMAP:
+        encode(c)
 
 
-class EncodeRowTests(unittest.TestCase):
+def test_encode_character_codes():
+    for i in CHARCODES:
+        encode(f"{{{i}}}")
+
+
+def test_encode_bad_characters():
+    pytest.raises(ValueError, encode, "<>").match("unsupported character: <")
+    pytest.raises(ValueError, encode, "{99}").match("unsupported character code: 99")
+    pytest.raises(ValueError, encode, "{20").match("missing }")
+    pytest.raises(ValueError, encode, "{999}").match("missing }")
+
+
+class TestEncodeRow:
     def test_maximum_length(self):
         encode_row("a" * 21 + "{10}")
-        self.assertRaisesRegex(Exception, "30 characters", encode_row, "a" * 30)
-        self.assertRaisesRegex(
-            Exception, "23 characters", encode_row, "a" * 22 + "{10}"
-        )
+        pytest.raises(Exception, encode_row, "a" * 30).match("30 characters")
+        pytest.raises(Exception, encode_row, "a" * 22 + "{10}").match("23 characters")
 
     def test_align_left(self):
-        self.assertEqual(
-            [1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            encode_row("abc", align="left"),
-        )
+        chars = [1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        assert encode_row("abc", align="left") == chars
 
     def test_align_center(self):
-        self.assertEqual(
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            encode_row("abc", align="center"),
-        )
+        chars = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        assert encode_row("abc", align="center") == chars
 
     def test_align_right(self):
-        self.assertEqual(
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3],
-            encode_row("abc", align="right"),
-        )
+        chars = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3]
+        assert encode_row("abc", align="right") == chars
 
     def test_unknown_alignment(self):
-        self.assertRaisesRegex(
-            ValueError, "unknown alignment", encode_row, "a", align="unknown"
-        )
+        with pytest.raises(ValueError, match="unknown alignment"):
+            encode_row("a", align="unknown")
 
     def test_fill(self):
-        self.assertEqual(
-            [1, 2, 3] + [int(Color.GREEN)] * (COLS - 3),
-            encode_row("abc", align="left", fill=Color.GREEN),
-        )
+        chars = [1, 2, 3] + [int(Color.GREEN)] * (COLS - 3)
+        assert encode_row("abc", align="left", fill=Color.GREEN) == chars
 
 
-class EncodeTextTests(unittest.TestCase):
+class TestEncodeText:
     def test_newlines(self):
-        self.assertEqual(
-            [
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            ],
-            encode_text("a\nb\nc"),
-        )
+        chars = [
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+        assert encode_text("a\nb\nc") == chars
 
     def test_align_left(self):
-        self.assertEqual(
-            # fmt: off
-            [
-                [20, 8, 9, 19, 0, 9, 19, 0, 1, 0, 20, 5, 19, 20, 0, 15, 6, 0, 12, 5, 6, 20],
-                [1, 12, 9, 7, 14, 13, 5, 14, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [25, 1, 25, 37, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            ],
-            # fmt: on
-            encode_text("This is a test of left alignment\nYay!", align="left"),
+        chars = [
+            [20, 8, 9, 19, 0, 9, 19, 0, 1, 0, 20, 5, 19, 20, 0, 15, 6, 0, 12, 5, 6, 20],
+            [1, 12, 9, 7, 14, 13, 5, 14, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [25, 1, 25, 37, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+        assert (
+            encode_text("This is a test of left alignment\nYay!", align="left") == chars
         )
 
     def test_align_center(self):
-        self.assertEqual(
-            # fmt: off
-            [
-                [0, 0, 20, 8, 9, 19, 0, 9, 19, 0, 1, 0, 20, 5, 19, 20, 0, 15, 6, 0, 0, 0],
-                [0, 0, 0, 3, 5, 14, 20, 5, 18, 0, 1, 12, 9, 7, 14, 13, 5, 14, 20, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 25, 1, 25, 37, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            ],
-            # fmt: on
-            encode_text("This is a test of center alignment\nYay!", align="center"),
+        chars = [
+            [0, 0, 20, 8, 9, 19, 0, 9, 19, 0, 1, 0, 20, 5, 19, 20, 0, 15, 6, 0, 0, 0],
+            [0, 0, 0, 3, 5, 14, 20, 5, 18, 0, 1, 12, 9, 7, 14, 13, 5, 14, 20, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 25, 1, 25, 37, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+        assert (
+            encode_text("This is a test of center alignment\nYay!", align="center")
+            == chars
         )
 
     def test_align_right(self):
-        self.assertEqual(
-            # fmt: off
-            [
-                [0, 0, 0, 0, 0, 20, 8, 9, 19, 0, 9, 19, 0, 1, 0, 20, 5, 19, 20, 0, 15, 6],
-                [0, 0, 0, 0, 0, 0, 0, 18, 9, 7, 8, 20, 0, 1, 12, 9, 7, 14, 13, 5, 14, 20],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 25, 1, 25, 37],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            ],
-            # fmt: on
-            encode_text("This is a test of right alignment\nYay!", align="right"),
+        chars = [
+            [0, 0, 0, 0, 0, 20, 8, 9, 19, 0, 9, 19, 0, 1, 0, 20, 5, 19, 20, 0, 15, 6],
+            [0, 0, 0, 0, 0, 0, 0, 18, 9, 7, 8, 20, 0, 1, 12, 9, 7, 14, 13, 5, 14, 20],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 25, 1, 25, 37],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+        assert (
+            encode_text("This is a test of right alignment\nYay!", align="right")
+            == chars
         )
 
     def test_valign_top(self):
-        self.assertEqual(
-            [
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            ],
-            encode_text("a\nb", valign="top"),
-        )
+        chars = [
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+        assert encode_text("a\nb", valign="top") == chars
 
     def test_valign_middle(self):
-        self.assertEqual(
-            [
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            ],
-            encode_text("a\nb", valign="middle"),
-        )
+        chars = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+        assert encode_text("a\nb", valign="middle") == chars
 
     def test_valign_bottom(self):
-        self.assertEqual(
-            [
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            ],
-            encode_text("a\nb", valign="bottom"),
-        )
+        chars = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+        assert encode_text("a\nb", valign="bottom") == chars
 
     def test_valign_none(self):
-        self.assertEqual(
-            [
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            ],
-            encode_text("a\nb", valign=None),
-        )
+        chars = [
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+        assert encode_text("a\nb", valign=None) == chars
 
     def test_unknown_alignments(self):
-        self.assertRaisesRegex(
-            ValueError, "unknown alignment", encode_text, "a", align="unknown"
-        )
-        self.assertRaisesRegex(
-            ValueError, "unknown vertical alignment", encode_text, "a", valign="unknown"
-        )
+        with pytest.raises(ValueError, match="unknown alignment"):
+            encode_text("a", align="unknown")
+
+        with pytest.raises(ValueError, match="unknown vertical alignment"):
+            encode_text("a", valign="unknown")
 
     def test_maximum_rows(self):
-        self.assertRaisesRegex(
-            ValueError, f"results in {ROWS + 1} lines", encode_text, "a\n" * (ROWS + 1)
-        )
+        with pytest.raises(ValueError, match=f"results in {ROWS + 1} lines"):
+            encode_text("a\n" * (ROWS + 1))
 
     def test_margin(self):
-        self.assertEqual(
-            [
-                [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-                [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            ],
-            encode_text("a" * COLS, margin=1),
-        )
+        chars = [
+            [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+            [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+        assert encode_text("a" * COLS, margin=1) == chars
 
     def test_fill(self):
-        self.assertEqual(
-            [
-                [1, 2, 3] + [int(Color.GREEN)] * (COLS - 3),
-                [Color.GREEN] * COLS,
-                [Color.GREEN] * COLS,
-                [Color.GREEN] * COLS,
-                [Color.GREEN] * COLS,
-                [Color.GREEN] * COLS,
-            ],
-            encode_text("abc", align="left", fill=Color.GREEN),
-        )
+        chars = [
+            [1, 2, 3] + [int(Color.GREEN)] * (COLS - 3),
+            [Color.GREEN] * COLS,
+            [Color.GREEN] * COLS,
+            [Color.GREEN] * COLS,
+            [Color.GREEN] * COLS,
+            [Color.GREEN] * COLS,
+        ]
+        assert encode_text("abc", align="left", fill=Color.GREEN) == chars
 
     def test_breaks(self):
-        self.assertEqual(
+        chars = [
             # fmt: off
-            [
-                [23, 15, 18, 4, 0, 23, 15, 18, 4, 0, 23, 15, 18, 4, 0, 23, 15, 18, 4, 0, 0, 0],
-                [23, 15, 18, 4, 0, 23, 15, 18, 4, 0, 23, 15, 18, 4, 0, 23, 15, 18, 4, 0, 0, 0],
-                [23, 15, 18, 4, 0, 23, 15, 18, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            ],
+            [23, 15, 18, 4, 0, 23, 15, 18, 4, 0, 23, 15, 18, 4, 0, 23, 15, 18, 4, 0, 0, 0],
+            [23, 15, 18, 4, 0, 23, 15, 18, 4, 0, 23, 15, 18, 4, 0, 23, 15, 18, 4, 0, 0, 0],
+            [23, 15, 18, 4, 0, 23, 15, 18, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             # fmt: on
-            encode_text(" ".join(["word"] * 10)),
-        )
+        ]
+        assert encode_text(" ".join(["word"] * 10)) == chars
