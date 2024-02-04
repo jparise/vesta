30f4ebc42933cd5a54da7f0ddac39596a1a56859
Jon Parise <jon@indelible.org>
2024-02-04 10:43:10 -0800

    Add test coverage for vbml.Component

---
+++ tests/test_vbml.py
@@ -0,0 +1,40 @@
+from vesta.vbml import Component
+
+
+class TestComponent:
+    def test_template(self):
+        assert Component("template").asdict() == {"template": "template"}
+
+    def test_style(self):
+        assert Component("template", style={}).asdict() == {"template": "template"}
+        assert Component(
+            "template",
+            style={"height": 4, "width": 10, "justify": "left", "align": "top"},
+        ).asdict() == {
+            "template": "template",
+            "style": {"height": 4, "width": 10, "justify": "left", "align": "top"},
+        }
+
+    def test_height(self):
+        assert Component("template", height=4).asdict() == {
+            "template": "template",
+            "style": {"height": 4},
+        }
+
+    def test_width(self):
+        assert Component("template", width=10).asdict() == {
+            "template": "template",
+            "style": {"width": 10},
+        }
+
+    def test_justify(self):
+        assert Component("template", justify="left").asdict() == {
+            "template": "template",
+            "style": {"justify": "left"},
+        }
+
+    def test_align(self):
+        assert Component("template", align="top").asdict() == {
+            "template": "template",
+            "style": {"align": "top"},
+        }
